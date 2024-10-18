0214100fc5cc3da570026877853c2877a5867a1a
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2024-10-17 21:08:24 -0500

    feat: add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,11 +6,25 @@ export default defineNuxtConfig({
     '@primevue/nuxt-module',
     '@nuxtjs/tailwindcss',
     '@nuxt/eslint',
+    '@vueuse/nuxt',
+    '@formkit/auto-animate',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate',
+    '@nuxtjs/color-mode',
+    '@nuxt/devtools',
+    '@nuxt/image',
   ],
   devtools: { enabled: true },
   css: [
     'primeicons/primeicons.css',
   ],
+
+  colorMode: {
+    preference: 'light',
+  },
+  alias: {
+    pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs',
+  },
   compatibilityDate: '2024-04-03',
   primevue: {
     options: {
