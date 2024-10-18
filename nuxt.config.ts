e1296bf447120228ae9595405a5e2f6cf3f95b4c
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2024-10-17 20:40:56 -0500

    feat: finish tailwind and primevue setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/lara';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  css: [
+    'primeicons/primeicons.css'
+  ],
   devtools: { enabled: true },
-  modules: ['@primevue/nuxt-module']
+  modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
+  primevue: {
+    options: {
+      ripple: true,
+      inputVariant: 'outlined',
+      theme: {
+        preset: Aura,
+      },
+    },
+  }
 })
